fc591502889a1fe792bb949637e10db64b700018
nishidy <nishidyatutokyo@gmail.com>
2014-10-22 02:01:13 +0900

    Add color notification

--- js/my.js
+++ js/my.js
@@ -9,6 +9,19 @@ function deleteElement(id){
 	elem.removeChild(firstChild);
 }
 
+function ripple(c,k){
+	if(c>255) return;
+	setTimeout(function(){ripple(c+1,k)},10);
+	if(k==0){
+		document.bgColor = "#"+c.toString(16)+c.toString(16)+"ff";
+	}else if(k==1){
+		document.bgColor = "#"+c.toString(16)+"ff"+c.toString(16);
+	}else{
+		document.bgColor = "#"+"ff"+c.toString(16)+c.toString(16);
+	}
+}
+
+
 function makeEachQuery(){
 	deleteElement("disp_parent");
 
@@ -20,8 +33,9 @@ function makeEachQuery(){
 }
 
 function query(i,num,field,artext){
+	if(i>0) ripple(230,i%3);
 	if(i==num) return;
-	console.log(i,artext[i]);
+	//console.log(i,artext[i]);
 
 	tag_div = "disp"+String(i);
 	new_div = document.createElement("div");
@@ -31,7 +45,8 @@ function query(i,num,field,artext){
 	$('div#'+tag_div).load(
 			'/trans',
 			{words:artext[i]},
-			function(a,b,c){if(i<num) query(i+1,num,field,artext)}
+			function(responseText, status, XMLHttpRequest)
+			  {if(i<num) query(i+1,num,field,artext)}
 	);
 }
 
